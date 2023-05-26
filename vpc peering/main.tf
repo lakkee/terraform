963fc4ae05d56a8cc5ef6e03668357c032d9db6e
vamsi <vamsikrishnaslsf@gmail.com>
2023-05-26 17:29:36 +0530

    created vpc peering

--- vpc peering/main.tf
+++ vpc peering/main.tf
@@ -1,8 +1,60 @@
-resource "aws_instance" "my_vm" {
-  ami                       = "ami-0889a44b331db0194"
-  instance_type             = "t2.micro"
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "aws_vpc" "vpc2" {
+  cidr_block = "172.168.0.0/16"
+}
+
+resource "aws_subnet" "frontend" {
+  vpc_id     = aws_vpc.vpc1
+  cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "My EC2 instance"
+    Name = "frontend"
+  }
+}
+
+resource "aws_subnet" "backend" {
+  vpc_id     = aws_vpc.vpc1
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_security_group" "frontend_nsg" {
+  vpc_id = aws_vpc.vpc1
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "backend_nsg" {
+  vpc_id = aws_vpc.vpc1
+
+  ingress {
+    from_port   = 1433
+    to_port     = 1433
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
