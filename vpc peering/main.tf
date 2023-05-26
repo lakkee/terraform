75efe4adb805e5a5b3b2ecdb88e6da4421f8d173
vamsi <vamsikrishnaslsf@gmail.com>
2023-05-26 17:57:01 +0530

    created vpc peering

--- vpc peering/main.tf
+++ vpc peering/main.tf
@@ -58,3 +58,67 @@ resource "aws_security_group" "backend_nsg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+
+resource "aws_subnet" "dmz_subnet" {
+  vpc_id                  = "aws_vpc.vpc2"
+  cidr_block              = "172.168.1.0/16"
+}
+
+resource "aws_subnet" "mgmt_subnet" {
+  vpc_id                  = "aws_vpc.vpc2"
+  cidr_block              = "172.168.2.0/16"
+}
+
+resource "aws_security_group" "dmz_nsg" {
+
+  vpc_id ="aws_vpc.vpc2"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "mgmt_nsg" {
+
+  vpc_id = "aws_vpc.vpc2"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
