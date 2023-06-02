8fe1f0fc81b10f21fa7de6efdd5a07d5f10c837b
vamsi <vamsikrishnaslsf@gmail.com>
2023-06-02 09:29:13 +0530

    created vpc peering

--- vpc peering/main.tf
+++ vpc peering/main.tf
@@ -1,15 +1,15 @@
 resource "aws_vpc" "vpc1" {
-  cidr_block = "var.vpc1_cidr_block"
+  cidr_block = var.vpc1_cidr_block
 }
 
 resource "aws_vpc" "vpc2" {
-  cidr_block ="var.vpc2_cidr_block"
+  cidr_block =var.vpc2_cidr_block
 }
 
 
 resource "aws_subnet" "frontend" {
   vpc_id     = "aws_vpc.vpc1.id"
-  cidr_block = "var.frontend_subnet_cidr_block"
+  cidr_block = var.frontend_subnet_cidr_block
 
   tags = {
     Name = "frontend"
@@ -18,7 +18,7 @@ resource "aws_subnet" "frontend" {
 
 resource "aws_subnet" "backend" {
   vpc_id     = "aws_vpc.vpc1.id"
-  cidr_block = " var.backend_subnet_cidr_block"
+  cidr_block = var.backend_subnet_cidr_block
 
   tags = {
     Name = "backend"
@@ -30,7 +30,7 @@ resource "aws_security_group" "frontend_nsg" {
   vpc_id = "aws_vpc.vpc1.id"
 
   dynamic "ingress" {
-    for_each = "var.frontend_inbound_ports"
+    for_each = var.frontend_inbound_ports
     content {
       from_port   = ingress.value
       to_port     = ingress.value
@@ -45,7 +45,7 @@ resource "aws_security_group" "backend_nsg" {
   vpc_id = "aws_vpc.vpc1.id"
 
   dynamic "ingress" {
-    for_each =" var.backend_inbound_ports"
+    for_each =var.backend_inbound_ports
     content {
       from_port   = ingress.value
       to_port     = ingress.value
@@ -57,12 +57,12 @@ resource "aws_security_group" "backend_nsg" {
 
 resource "aws_subnet" "dmz_subnet" {
   vpc_id                  = "aws_vpc.vpc2.id"
-  cidr_block              = "var.dmz_subnet_cidr_block"
+  cidr_block              = var.dmz_subnet_cidr_block
 }
 
 resource "aws_subnet" "mgmt_subnet" {
   vpc_id                  = "aws_vpc.vpc2.id"
-  cidr_block              = "var.mgmt_subnet_cidr_block"
+  cidr_block              = var.mgmt_subnet_cidr_block
 }
 
 resource "aws_security_group" "dmz_nsg" {
